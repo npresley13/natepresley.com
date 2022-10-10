df7d896c7c546c61975a04c7ec3024e0dee6a14c
Nathan Presley <npresley13@gmail.com>
2022-10-10 16:56:07 -0400

    updates to copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,14 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Homepage">
-      <p>This is the intro to Nathan Presley</p>
+      <p>
+        Allow me to re-introduce myself,<br/>
+        my name is Nate.<br/>
+        N to the A Te.<br/>
+        I used to move cabinets by the L-Bs.<br/>
+        I guess even back then you could call me:<br/>
+        SEO of the S-H T-L-D.<br/>
+      </p>
     </Layout>
   )
 }
