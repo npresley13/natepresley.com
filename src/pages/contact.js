0c1490bb5311186c90d9090d5f30a33e9b37938f
Nathan Presley <npresley13@gmail.com>
2022-10-12 23:48:24 -0400

    added contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,29 @@ import Layout from '../components/layout'
 const contactPage = () => {
     return (
         <Layout pageTitle="Contact Me">
-            <p>Better yet don't contact me.</p>
+            <form method="post" action="#">
+                <div>
+                    <label>
+                        First Name
+                        <input type="text" name="first-name" id="first-name" />
+                    </label>
+                    <label>
+                        Last Name
+                        <input type="text" name="last-name" id="last-name" />
+                    </label>
+                </div>
+                <label>
+                    Email
+                    <input type="email" name="email" id="email" />
+                </label>
+                <label>
+                    Message
+                    <textarea name="message" id="message" rows="5" />
+                </label>
+                
+                <button className="button" type="submit">Send</button>
+                
+            </form>
         </Layout>
     )
 }
