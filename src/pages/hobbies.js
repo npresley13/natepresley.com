30f986ad06d1d838ce539e24d21ea7ab5ebb4dbb
Nathan Presley <npresley13@gmail.com>
2022-10-17 23:02:32 -0400

    added hobbies cards

--- src/pages/hobbies.js
+++ src/pages/hobbies.js
@@ -4,7 +4,48 @@ import Layout from '../components/layout'
 const AboutPage = () => {
     return(
        <Layout pageTitle="Nate's Hobbies" metaDescription="Learn more about Nathan Presley's personal hobbies.">
-            <p>This is the About section for Nathan Presley.</p>
+            <div>
+                <div>Image</div>
+                <h2>Volleyball</h2>
+                <div>Days a Week</div>
+                <div>Years</div>
+                <p>description</p>
+            </div>
+            <div>
+                <div>Image</div>
+                <h2>Volleyball</h2>
+                <div>Days a Week</div>
+                <div>Years</div>
+                <p>description</p>
+            </div>
+            <div>
+                <div>Image</div>
+                <h2>Volleyball</h2>
+                <div>Days a Week</div>
+                <div>Years</div>
+                <p>description</p>
+            </div>
+            <div>
+                <div>Image</div>
+                <h2>Volleyball</h2>
+                <div>Days a Week</div>
+                <div>Years</div>
+                <p>description</p>
+            </div>
+            <div>
+                <div>Image</div>
+                <h2>Volleyball</h2>
+                <div>Days a Week</div>
+                <div>Years</div>
+                <p>description</p>
+            </div>
+            <div>
+                <div>Image</div>
+                <h2>Volleyball</h2>
+                <div>Days a Week</div>
+                <div>Years</div>
+                <p>description</p>
+            </div>
        </Layout>
     )
 }
