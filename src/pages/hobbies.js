abf338fe984a94a4c8cd37e9931f6cae4c6a3b7c
Nathan Presley <npresley13@gmail.com>
2022-10-18 22:58:19 -0400

    added image to website

--- src/pages/hobbies.js
+++ src/pages/hobbies.js
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
     return(
        <Layout pageTitle="Nate's Hobbies" metaDescription="Learn more about Nathan Presley's personal hobbies.">
             <div>
-                <div></div>
+                <div><StaticImage src="../images/volleyball.png" alt="nate winning a volleyball tournament"/></div>
                 <h2>Volleyball</h2>
-                <div>Days a Week</div>
-                <div>Years</div>
+                <div>9 Years</div>
                 <p>description</p>
             </div>
             <div>
